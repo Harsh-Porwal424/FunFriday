585038a85a0edd1071748612faf89cd125ba6b99
Harsh Porwal <harshporwal@Harshs-MacBook-Air-2.local>
2025-07-24 21:32:09 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  const port = parseInt(process.env.PORT || '3000', 10);
+  const HOST = '127.0.0.1'; // or '0.0.0.0' for external access
+  server.listen(port, HOST, () => {
+    console.log(`Server running at http://${HOST}:${port}/`);
   });
 })();
